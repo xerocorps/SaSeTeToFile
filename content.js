0e08af6b613f029780d01e7ca89cedb671f3d096
Y2hhbmRhbiBwYXRoYWs= <71752348+xerocorps@users.noreply.github.com>
2024-05-11 00:19:14 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,5 @@
 var flag;
 var show;
-
-
-
-
 var bubbleDOM = document.createElement('input');
 
 bubbleDOM.setAttribute('type', 'button');
@@ -13,23 +9,12 @@ bubbleDOM.setAttribute('class', 'selection_bubble');
 bubbleDOM.setAttribute('id', "btn");
 document.body.appendChild(bubbleDOM);
 
-var inputDOM = document.createElement('input');
-
-inputDOM.setAttribute('type', 'button');
-
-
-inputDOM.setAttribute('class', 'selection_bubble');
-inputDOM.setAttribute('id', "btn");
-document.body.appendChild(inputDOM);
-
 
 //Close the bubble when we click on the screen.
 document.addEventListener('mousedown', function(e) {
     if (bubbleDOM.id != e.target.id && bubbleDOM.style.visibility == 'visible') {
         bubbleDOM.removeAttribute("value");
-        inputDOM.removeAttribute("value");
         bubbleDOM.style.visibility = 'hidden';
-        inputDOM.style.visibility = 'hidden';
     }
 }, false);
 
@@ -57,47 +42,36 @@ function renderBubble(mouseX, mouseY, pageInfo) {
 
 function showit(mouseX, mouseY, pageInfo) {
     if (bubbleDOM.hasAttribute('value') == false) {
-        bubbleDOM.setAttribute('value', "Add to file");
-        inputDOM.setAttribute('value', "Save file");
+        bubbleDOM.setAttribute('value', "📝 Add to file");
 
         bubbleDOM.style.top = mouseY + 'px';
         bubbleDOM.style.left = mouseX + 'px';
         bubbleDOM.style.visibility = 'visible';
-        inputDOM.style.top = mouseY + 'px';
-        inputDOM.style.left = (mouseX + 102) + 'px';
-        inputDOM.style.visibility = 'visible';
 
         bubbleDOM.onclick = function() {
             addToFile(pageInfo);
-        };
-        inputDOM.onclick = function() {
-            saveFile(pageInfo);
         };
     }
 }
 
 function saveFile(pageInfo) {
     bubbleDOM.style.visibility = 'hidden';
-    inputDOM.style.visibility = 'hidden';
 
     chrome.runtime.sendMessage({
         task: 'saveFile',
         pageInfo: pageInfo
     }, function() {
         bubbleDOM.removeAttribute("value");
-        inputDOM.removeAttribute("value");
     });
 }
 
 function addToFile(pageInfo) {
     bubbleDOM.style.visibility = 'hidden';
-    inputDOM.style.visibility = 'hidden';
 
     chrome.runtime.sendMessage({
         task: 'addToFile',
         pageInfo: pageInfo
     }, function() {
         bubbleDOM.removeAttribute("value");
-        inputDOM.removeAttribute("value");
     });
 }
